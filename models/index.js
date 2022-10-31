c9603b85a05e9a0e0c60229bcf1862f1dccc389d
Greg Stickel <gregory.stickel@live.com>
2022-10-31 13:49:20 -0400

    Updated 1:50pm 10-31-22

--- models/index.js
+++ models/index.js
@@ -1,41 +1,5 @@
 const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Tdf extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  }
-  Tdf.init(
-    {
-      id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-      },
-     stage: DataTypes.STRING,
-     year: DataTypes.INTEGER,
-     distance: DataTypes.INTEGER,
-     origin: DataTypes.STRING,
-     destination: DataTypes.STRING,
-     type: DataTypes.INTEGER,
-     winner: DataTypes.STRING,
-     winner_country: DataTypes.STRING,
-
-    },
-    {
-      // options
-      sequelize,
-      modelName: 'Tdf',
-      tableName: 'tdf',
-      underscore: true,
-      createdAt: 'date_created',
-      timestamps: false
-    },
-  );
-  return Tdf;
+  
+  
 };
